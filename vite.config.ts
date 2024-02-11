c52ea2535a6caf4afdce5f0981428d3eafb0243d
Tamas Katona <tamaskatona613@gmail.com>
2024-02-11 22:11:48 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import vite_react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/web-audio-oscillator',
     server: {
         open: true,
     },
